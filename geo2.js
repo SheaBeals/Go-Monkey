399e53a4ce47e20f15b777f0a6c563daeca22d39
SheaBeals <71564638+SheaBeals@users.noreply.github.com>
2020-09-19 14:22:19 -0400

    Add files via upload

---
+++ geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "KITCHENER",
+      'continent': "NA",
+      'country': "CA",
+      'region': "ON"
+    },
+    'ip':"216.209.248.166"
+  }]);
+})
+//
+()
+
+;
